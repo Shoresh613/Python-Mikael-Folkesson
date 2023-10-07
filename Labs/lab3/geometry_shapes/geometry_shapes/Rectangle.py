1c393bfe9ad6498c76b27d4340c46f7ddcf6a3f2
Shoresh613 <mikael.folkesson@gmail.com>
2023-10-07 15:23:00 +0200

    rectangle properties

--- Labs/lab3/geometry_shapes/geometry_shapes/Rectangle.py
+++ Labs/lab3/geometry_shapes/geometry_shapes/Rectangle.py
@@ -2,7 +2,7 @@ from Common_supershape import Common_supershape
 
 class Rectangle(Common_supershape):
     def __init__(self, x=0, y=0, side1=1,side2=1):
-        self.x = x
+        self._x = x
         self.y = y
         self.side1 = side1
         self.side2 = side2
@@ -21,4 +21,46 @@ class Rectangle(Common_supershape):
         return f"Rectangle({self.x, self.y, self.side1, self.side2})"
 
     def __str__(self) -> str:
-        return super().__str__() + f": Center point: {self.x,self.y}, radius: {self.radius}, circumference: {self.circumference}, area: {self.area}"
+        return super().__str__() + f": Center point: {self.x,self.y}, width: {self.side1}, height {self.side2}"
+
+# Setters and getters beyond this point
+#######################################
+
+    # Getter method
+    @property
+    def x(self):
+        return self._x
+
+    # Setter method
+    @x.setter
+    def red(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    # Setter method
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    @property
+    def side1(self):
+        return self._side1
+
+    # Setter method
+    @side1.setter
+    def side1(self, side1):
+        self._side1 = side1
+
+    @property
+    def side2(self):
+        return self._side2
+
+    # Setter method
+    @side2.setter
+    def side2(self, side2):
+        self._side2 = side2
+
+
